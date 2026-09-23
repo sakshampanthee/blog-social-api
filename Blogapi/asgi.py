8fd754c8e98dd4cc11856236637e202a3c128589
sakshampanthee <sakshampanthi2063@gmail.com>
2026-09-23 14:23:16 +0545

    Fix ASGI middleware initialization

--- Blogapi/asgi.py
+++ Blogapi/asgi.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from Blog.middleware import JWTAuthMiddleware
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blogapi.settings')
 
 django_asgi_app = get_asgi_application()
 
 from Blog.routing import websocket_urlpatterns
+from Blog.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
